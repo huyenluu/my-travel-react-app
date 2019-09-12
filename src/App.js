f3958014df67b475ed19b36e1c2fd7b137ae04a3
Huyen Luu <haihuyen.luu@gmail.com>
2019-09-12 23:52:41 +0300

    quest no.2 & style with material UI

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import cinquePic from './cinque-terre.jpg';
-import provencePic from './provence.jpg';
+
 import './App.css';
 
 import Travel from "./Travel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3> This is my first react website. </h3>
-      </header>
-      <Travel
-        photo = {cinquePic}
-        destination = "Cinque Terre"
-        country = "Italy"
-        distance ="1918 km from Bucharest"
-      />  
-      <Travel
-        photo = {provencePic}
-        destination = "Provence"
-        country = "France"
-        distance ="2198 km from Bucharest"
-      />  
+    <div >
+
+      <Travel/>  
+
     </div>
     
   );
--- src/Travel.js
+++ src/Travel.js
@@ -1,20 +1,110 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
-const Travel = ({photo, destination, country,distance}) => (
-  <div className = 'container'>
+import cinqueTerre from './cinque-terre.jpg';
+import provence from './provence.jpg';
+import amalfi from './amalfi.jpg';
+import hoian from './hoian.jpg';
+import krabi from './krabi.jpg';
+import newYork from './new-york.jpg';
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    background: theme.background,
+  },
+  gridList: {
+    width: 1500,
+  },
+  header: {
+    textAlign: "center", 
+    fontSize: 50,
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    textTransform: 'capitalize',
     
-    <img
-    src= {photo}
-    alt={destination}
-    className = 'picture'
-    />
-     
-    <div className = 'text-block'>
-      <h2>{destination}</h2>
-      <p>{country}</p>
-      <p>{distance}</p> 
-    </div>
-  </div>
-);
+  }
+}));
 
-export default Travel;
+
+const tileData = [
+  {
+    img: cinqueTerre,
+    destination: 'Cinque Terre',
+    country: 'Italy',
+    distance: '200km',
+  },
+  {
+    img: provence,
+    destination: 'Provence',
+    country: 'France',
+    distance: '200km',
+  },
+  {
+    img: newYork,
+    destination: 'New York',
+    country: 'USA',
+    distance: '200km',
+  },
+  {
+    img: amalfi,
+    destination: 'Amalfi',
+    country: 'Italy',
+    distance: '200km',
+  },
+  {
+    img: krabi,
+    destination: 'Krabi',
+    country: 'Thailand',
+    distance: '200km',
+  },
+  {
+    img: hoian,
+    destination: 'Hoi An',
+    country: 'Viet Nam',
+    distance: '200km',
+  },
+ 
+]
+
+
+export default function TitlebarGridList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={350} cols= {3} spacing = {10} className={classes.gridList}>
+        <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+          <ListSubheader color ='primary' component="h2" className={classes.header} > travel with me </ListSubheader>
+        </GridListTile>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img} className={classes.tile}>
+            <img src={tile.img} alt={tile.destination} />
+            <GridListTileBar
+              title = {tile.destination}
+              subtitle={
+               <>
+                 <div> Country: {tile.country}</div>
+                 <div> Distance: {tile.distance}</div>
+              </> 
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
+}
